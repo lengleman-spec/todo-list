fce36fa8a83288be187842e6a830b080961f3c78
lengleman-spec <191269315+lengleman-spec@users.noreply.github.com>
2026-03-08 17:05:55 -0400

    Write class for each entire project - comprised of smaller todo tasks.

--- project.js
+++ project.js
@@ -1,1 +1,22 @@
 // Project class (list of todos) goes here
+export class Project {
+  constructor(name) {
+    this.name = name;
+    this.todos = [];
+  }
+
+  // adds item to the end of the list
+  addTodo(todo) {
+    this.todos.push(todo);
+  }
+
+  // removes item at index X
+  deleteTodo(index) {
+    this.todos.splice(index);
+  }
+
+  // returns the projects todos to be used later
+  getTodos() {
+    return this.todos;
+  }
+}
