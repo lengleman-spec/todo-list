cc967119ce784573a7eefcca9fb15fb4fea13e9a
lengleman-spec <191269315+lengleman-spec@users.noreply.github.com>
2026-03-09 13:37:48 -0400

    Add event listener for submit and prevent the page from reloading.

--- dom.js
+++ dom.js
@@ -1,3 +1,8 @@
 // DOM manipulation logic (rendering, event listeners)
 // import the app module:
 import { App } from "./app.js";
+const todoForm = document.getElementById("add-todo-form");
+todoForm.addEventListener("submit", (event) => {
+  //stop the page from reloading with:
+  event.preventDefault();
+});
