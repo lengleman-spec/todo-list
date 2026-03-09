cc274972b494f3121d0c3270c4d2e94918df4992
lengleman-spec <191269315+lengleman-spec@users.noreply.github.com>
2026-03-09 12:48:56 -0400

    Add logic to create a variable to store projects in. Create function to add projects to that project variable.

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
 // Main app logic (managing multiple projects)
+import { Project } from "./project.js";
+
+// use IIFE so that the data is private
+export const App = (() => {
+  // store all projects
+  let projects = [];
+
+  const defaultProject = new Project("Default");
+  projects.push(defaultProject);
+
+  // add projects
+  function addProject(name) {
+    const newProject = new Project(name);
+    projects.push(newProject);
+  }
+
+  // function to get all projects for the DOM
+  function getProjects() {
+    return projects;
+  }
+
+  // return the public functions we want accessible
+  return { addProject, getProjects };
+})();
+
+window.App = App;
