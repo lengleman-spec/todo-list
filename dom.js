4523f207e990c7fe569ba8c23ce32e0bf246b4db
lengleman-spec <191269315+lengleman-spec@users.noreply.github.com>
2026-03-09 14:07:35 -0400

    Add logic to get what the user input using .value. Create the todo using the App function and update the UI.

--- dom.js
+++ dom.js
@@ -5,4 +5,19 @@ const todoForm = document.getElementById("add-todo-form");
 todoForm.addEventListener("submit", (event) => {
   //stop the page from reloading with:
   event.preventDefault();
+
+  // Use .value to get the user's input
+  const title = document.getElementById("todo-title").value;
+  const description = document.getElementById("todo-desc").value;
+  const dueDate = document.getElementById("todo-due").value;
+  const priority = document.getElementById("todo-priority").value;
+
+  // Call the app function to create the todo:
+  App.addTodoToProject(0, title, description, dueDate, priority);
+
+  // Update the UI:
+  renderTodos(0);
+
+  // Reset the form so it's empty for the next todo:
+  todoForm.reset();
 });
